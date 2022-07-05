7eeda43dd66c940d11e907d1857f1ccd07d1db3e
Hectorovich <andrew.gutnik10@gmail.com>
2022-07-05 16:15:13 +0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -9,29 +9,6 @@ from django.views import generic
 from .models import Driver, Car, Manufacturer
 
 
-def login_view(request):
-    if request.method == "GET":
-        return render(request, "registration/login.html")
-    elif request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(username=username, password=password)
-
-        if user:
-            login(request, user)
-            return HttpResponseRedirect(reverse("taxi:index"))
-        else:
-            error_context = {
-                "errors": "Invalid credentials"
-            }
-            return render(request, "registration/login.html", context=error_context)
-
-
-def logout_view(request):
-    logout(request)
-    return render(request, "registration/logged_out.html")
-
-
 @login_required
 def index(request):
     """View function for the home page of the site."""

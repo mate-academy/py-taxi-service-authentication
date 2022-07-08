13ec72ef18812c0cddea34a03a1b933d8861bb2d
Ed Orlovskyy <fedorov12816@gmail.com>
2022-07-08 12:05:38 +0300

    Built_in_AuthSol

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "taxi",
-    "accounts"
 ]
 
 MIDDLEWARE = [
@@ -102,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = "taxi.Driver"
+
+LOGIN_REDIRECT_URL = "/taxi/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/

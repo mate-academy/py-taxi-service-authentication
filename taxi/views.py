2192c1686953a5d2ade46ffc697bf42e4eaf22a2
Hectorovich <andrew.gutnik10@gmail.com>
2022-07-09 12:22:27 +0300

    Fixed imports and added hidden input

--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,6 @@
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
 from django.views import generic
 
 from .models import Driver, Car, Manufacturer

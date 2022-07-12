824a1f8bc3aeb245d18e96ab296a4441043c6b4b
ILIAS <nossudak@gmail.com>
2022-07-12 16:16:30 +0300

    Fixed visit counter

--- taxi/views.py
+++ taxi/views.py
@@ -14,13 +14,13 @@ def index(request):
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
 
-    num_of_visits = request.session.get("num_of_visits", 1)
+    num_of_visits = request.session.get("num_of_visits", 0)
     request.session["num_of_visits"] = num_of_visits + 1
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
         "num_manufacturers": num_manufacturers,
-        "num_of_visits": num_of_visits
+        "num_of_visits": num_of_visits + 1
     }
 
     return render(request, "taxi/index.html", context=context)

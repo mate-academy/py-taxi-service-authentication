6c88c36c62b4f432ebea2681d2332ababb953744
mltdll <ilya.vyplavin@gmail.com>
2022-10-27 19:59:20 +0300

    Fix flake8 errors

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path("", include("taxi.urls", namespace="taxi")),
 
 
-    path('__debug__/', include('debug_toolbar.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

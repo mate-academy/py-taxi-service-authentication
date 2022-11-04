0c5493b7dbb2568e1b7daf2461d1ee8357942503
Alexander <alexzubkoff123@gmail.com>
2022-11-04 23:20:52 +0200

    Created custom auth with visit counter, login

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -21,5 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("taxi.urls", namespace="taxi")),
+    path("taxi/", include("taxi.urls", namespace="taxi")),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

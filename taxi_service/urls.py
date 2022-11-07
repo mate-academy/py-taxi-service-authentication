51e037aa95923c2b54495c635b6cf792f746db1c
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-11-07 23:06:39 +0200

    'py_taxi_service_authentication_2'

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", include("taxi.urls", namespace="taxi")),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

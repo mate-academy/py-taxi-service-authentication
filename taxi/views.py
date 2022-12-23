5140e669c6079392576d1836627ae61d330c0739
Denys Tereshchuk <terrya@ukr.net>
2022-12-23 18:29:42 +0400

    all unnecessary were removed

--- taxi/views.py
+++ taxi/views.py
@@ -9,18 +9,13 @@ from .models import Driver, Car, Manufacturer
 @login_required
 def index(request):
     """View function for the home page of the site."""
-
-    num_drivers = Driver.objects.count()
-    num_cars = Car.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
+    request.session["num_visits"] = request.session.get("num_visits", 0) + 1
 
     context = {
-        "num_drivers": num_drivers,
-        "num_cars": num_cars,
-        "num_manufacturers": num_manufacturers,
-        "num_visits": num_visits + 1
+        "num_drivers": Driver.objects.count(),
+        "num_cars": Car.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
+        "num_visits": request.session.get("num_visits")
     }
 
     return render(request, "taxi/index.html", context=context)
@@ -28,9 +23,6 @@ def index(request):
 
 class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     model = Manufacturer
-    context_object_name = "manufacturer_list"
-    queryset = Manufacturer.objects.all()
-    template_name = "taxi/manufacturer_list.html"
     paginate_by = 5
 
 

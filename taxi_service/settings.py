1a27da5bf01df55261f8bcb34e1c539e88b5358d
Natalie Goriela <natalie.goriela@gmail.com>
2023-03-25 18:47:34 +0200

    'Solution'

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -108,6 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "taxi.Driver"
 
+LOGIN_REDIRECT_URL = "http://127.0.0.1:8000/"
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 

f109fce1555eb32f94fc988a7da50d2c9a551973
Denys <deibukdenyscloud@gmail.com>
2023-09-24 15:54:00 +0300

    Restrict authenticated users' ability to access login page

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
+    path(
+        "accounts/login/",
+        views.LoginView.as_view(redirect_authenticated_user=True),
+        name="login",
+    ),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

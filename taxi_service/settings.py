1b3e9bcb9f8f2b8608d75b581cf2071e35600e0e
ORENYT <106102798+ORENYT@users.noreply.github.com>
2023-09-26 17:14:18 +0300

    Fixed 404 error (last test)

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "taxi",
 ]
 
+LOGIN_REDIRECT_URL = "/"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",

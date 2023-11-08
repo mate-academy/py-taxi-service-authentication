a7c91df3f3a403ffb8b015e14db90f462c3d71d5
Misha Shushman <endernagashushman@gmail.com>
2023-11-08 21:32:48 +0200

    Fixes

--- taxi/urls.py
+++ taxi/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
-    path("accounts/", include("django.contrib.auth.urls"))
 ]
 
 app_name = "taxi"

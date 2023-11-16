608b3afdcbe102ddd14b36ee828470371f20fd22
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-11-16 14:27:15 +0200

    Fixed pep8

--- taxi/views.py
+++ taxi/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Driver, Car, Manufacturer
 
+
 @login_required()
 def index(request):
     """View function for the home page of the site."""

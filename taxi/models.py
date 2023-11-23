5fc1f34f0690936519ccc5d8bf6c0b1be6fee83d
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-11-23 13:44:28 +0200

    fixed annotation return type in models.py

--- taxi/models.py
+++ taxi/models.py
@@ -29,5 +29,5 @@ class Car(models.Model):
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(Driver, related_name="cars")
 
-    def __str__(self) -> model:
-        return self.model
+    def __str__(self) -> str:
+        return f"{self.model}"

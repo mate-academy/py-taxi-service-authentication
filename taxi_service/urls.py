7682e4c2e3cbad58f251f4ad8abe49626ddc63d7
Viktor Deyneka <3lobites@gmail.com>
2023-11-23 22:27:14 +0200

    Solution2

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
     path("taxi/", include("taxi.urls", namespace="taxi")),
-    path("accounts/", include("django.contrib.auth.urls")),
+    # path("accounts/", include("django.contrib.auth.urls")),
+    path("login/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

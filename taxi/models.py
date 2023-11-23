b21b21e8f0b1b162d6063669e44794941a10845b
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-11-23 14:41:36 +0200

    models.py

--- taxi/models.py
+++ taxi/models.py
@@ -30,4 +30,4 @@ class Car(models.Model):
     drivers = models.ManyToManyField(Driver, related_name="cars")
 
     def __str__(self) -> str:
-        return f"{self.model}"
+        return self.model

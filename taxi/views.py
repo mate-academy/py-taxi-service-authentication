fe1555acaedb2e307b304a6cb5829e23eed55cc3
Alona <alyonkabagritskaya@gmail.com>
2023-12-18 15:26:01 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from .models import Driver, Car, Manufacturer
 
 
+@login_required()
 def index(request):
     """View function for the home page of the site."""
 
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "taxi",
+    "taxi_service",
 ]
 
 MIDDLEWARE = [

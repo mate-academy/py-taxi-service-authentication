1a4da6a16ec2a4d1d4405dcb4ec491edf58cdf8b
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-02-27 17:02:10 +0200

    Solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -18,8 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from taxi import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

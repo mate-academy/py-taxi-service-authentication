53308f650392a80938ee188fe6115cbe7795df6f
PythonZem <pythonzem@gmail.com>
2024-02-28 22:06:03 +0200

    solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "taxi",
-    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -108,6 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = "taxi.Driver"
+
+LOGIN_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/

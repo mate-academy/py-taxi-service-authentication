5dc6be5f329bbd9afb07b7d255173f94a2be74cf
Vlad <mr.darmstadtium@gmail.com>
2024-03-01 18:14:37 +0100

    some flake 8

--- taxi_service/wsgi.py
+++ taxi_service/wsgi.py
@@ -16,5 +16,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taxi_service.settings")
 
 application = get_wsgi_application()
 
-print(os.environ.get('DJANGO_SETTINGS_MODULE'))
-
+print(os.environ.get("DJANGO_SETTINGS_MODULE"))

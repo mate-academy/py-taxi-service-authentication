6bc972a82165d0662036bc6c6e458aa7c885cdef
twonumberfortyfives <sma4no.7@gmail.com>
2024-04-09 14:26:28 +0300

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -129,11 +129,7 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = "/"
 
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"

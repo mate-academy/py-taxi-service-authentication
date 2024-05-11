2c49e76bb2beb7d39a9a2072d5dbecea7e90e685
bujhm <Bujhm99@gmail.com>
2024-05-11 03:12:57 +0300

    Back to basics

--- taxi/models.py
+++ taxi/models.py
@@ -1,44 +1,33 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.urls import reverse
+from django.contrib.auth.models import AbstractUser
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
-    def __str__(self) -> models.CharField:
-        return self.name
-
     class Meta:
-        ordering = ("name",)
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} {self.country}"
 
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
 
     class Meta:
-        ordering = ("first_name", "last_name",)
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
 
-    def __str__(self) -> str:
-        return f"{self.username}: {self.first_name} {self.last_name}"
-
-    def get_absolute_url(self):
-        return reverse("taxi:driver-detail", args=[str(self.id)])
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
 
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
-    manufacturer = models.ForeignKey(
-        Manufacturer, on_delete=models.CASCADE, related_name="cars"
-    )
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(Driver, related_name="cars")
 
-    def __str__(self) -> models.CharField:
+    def __str__(self):
         return self.model
-
-    class Meta:
-        ordering = ("model",)
-
-    def get_absolute_url(self):
-        return reverse("taxi:car-detail", args=[str(self.pk)])

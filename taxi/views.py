ac212407a87e9eedf9e00c8968668c1fb619d42e
Dima <dima2000yan@gmail.com>
2024-07-10 18:35:16 +0300

    solution fix

--- taxi/views.py
+++ taxi/views.py
@@ -15,9 +15,9 @@ def index(request: HttpRequest) -> HttpResponse:
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
-    num_visits = request.session.get('num_visits', 0)
+    num_visits = request.session.get("num_visits", 0)
     num_visits += 1
-    request.session['num_visits'] = num_visits
+    request.session["num_visits"] = num_visits
 
     context = {
         "num_drivers": num_drivers,

58c4b40c5965648b3fcdd033f37b7f8d507e25f5
Dima <dima2000yan@gmail.com>
2024-07-15 16:35:26 +0300

    fixes

--- taxi/models.py
+++ taxi/models.py
@@ -18,7 +18,7 @@ class Driver(AbstractUser):
 
     class Meta:
         verbose_name = "driver"
-        verbose_name_plural = "registration"
+        verbose_name_plural = "drivers"
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpRequest
@@ -54,26 +53,3 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
     queryset = Driver.objects.prefetch_related("cars__manufacturer")
-
-
-def login_view(request: HttpRequest) -> HttpResponse:
-    if request.method == "GET":
-        return render(request, "registration/login.html")
-    elif request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(username=username, password=password)
-        if user:
-            login(request, user)
-        else:
-            error_context = {
-                "error": "invalid credentials"
-            }
-            return render(request,
-                          "registration/login.html",
-                          context=error_context)
-
-
-def logout_view(request: HttpRequest) -> HttpResponse:
-    logout(request)
-    return render(request, "registration/logged_out.html")

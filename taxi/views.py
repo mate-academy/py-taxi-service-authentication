244a2940944cacf711f110f9afba5721927e4030
unlie9 <unlie.devops9@gmail.com>
2024-07-18 08:41:52 +0300

    fixed imports and var num_visits

--- taxi/views.py
+++ taxi/views.py
@@ -1,9 +1,8 @@
+from taxi.models import (Driver, Car, Manufacturer)
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
-
-from .models import Driver, Car, Manufacturer
 
 
 @login_required
@@ -13,14 +12,14 @@ def index(request):
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
+    num_visits = request.session.get("num_visits", 0) + 1
+    request.session["num_visits"] = num_visits
 
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
         "num_manufacturers": num_manufacturers,
-        "num_visits": num_visits + 1,
+        "num_visits": num_visits
     }
 
     return render(request, "taxi/index.html", context=context)

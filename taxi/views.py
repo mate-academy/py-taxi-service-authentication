ef371d74d7a955b68f001fb34b4529fdffc2eba3
Zakotii <abc1234567892777@gmail.com>
2024-07-31 19:57:21 +0300

    Solution6

--- taxi/views.py
+++ taxi/views.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from pip._internal.utils import logging
 from .models import Car, Driver, Manufacturer
-from django.views.generic import ListView
-
-
-def index(request):
-    """View function for the home page of the site."""
-
-    num_drivers = Driver.objects.count()
-    num_cars = Car.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
-
-    context = {
-        "num_drivers": num_drivers,
-        "num_cars": num_cars,
-        "num_manufacturers": num_manufacturers,
-        "num_visits": num_visits,
-    }
-
-    return render(request, "taxi/index.html", context=context)
 
 
 class ManufacturerListView(LoginRequiredMixin, generic.ListView):
@@ -48,7 +28,7 @@ class CarDetailView(LoginRequiredMixin, generic.DetailView):
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
     queryset = Driver.objects.prefetch_related("cars__manufacturer")
-    emplate_name = "taxi/driver_detail.html"
+    template_name = "taxi/driver_detail.html"
 
 
 logger = logging.getLogger(__name__)
@@ -63,7 +43,6 @@ class IndexView(LoginRequiredMixin, TemplateView):
             self.request.session.create()
         num_visits = self.request.session.get("num_visits", 0)
         self.request.session["num_visits"] = num_visits + 1
-        logger.debug(f"num_visits: {num_visits + 1}")  # Добавить логирование
 
         context["num_visits"] = num_visits + 1
         context["num_cars"] = Car.objects.count()

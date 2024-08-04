4c6372f35c7d142a88cd2722b294cd2041bc4682
antonver <anton.verbovyi@gmail.com>
2024-08-04 22:22:13 +0200

    Done2

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('taxi.urls', namespace='taxi')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("taxi.urls", namespace="taxi")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

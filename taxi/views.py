c0e294851bc037d2027d2ea44ce67f705e423463
Ivan Vahanov <ivan.seeker6@gmail.com>
2024-08-12 05:28:45 +0300

    sol1

--- taxi/views.py
+++ taxi/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.views import generic
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 from .models import Driver, Car, Manufacturer
 
 
 def index(request):
-    """View function for the home page of the site."""
-
-    num_drivers = Driver.objects.count()
-    num_cars = Car.objects.count()
-    num_manufacturers = Manufacturer.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
-        "num_drivers": num_drivers,
-        "num_cars": num_cars,
-        "num_manufacturers": num_manufacturers,
+        "num_drivers": Driver.objects.count(),
+        "num_cars": Car.objects.count(),
+        "num_manufacturers": Manufacturer.objects.count(),
+        "num_visits": num_visits,
     }
 
     return render(request, "taxi/index.html", context=context)
 
 
+@method_decorator(login_required, name="dispatch")
 class ManufacturerListView(generic.ListView):
     model = Manufacturer
     context_object_name = "manufacturer_list"
@@ -27,21 +28,25 @@ class ManufacturerListView(generic.ListView):
     paginate_by = 5
 
 
+@method_decorator(login_required, name="dispatch")
 class CarListView(generic.ListView):
     model = Car
     paginate_by = 5
     queryset = Car.objects.select_related("manufacturer")
 
 
+@method_decorator(login_required, name="dispatch")
 class CarDetailView(generic.DetailView):
     model = Car
 
 
+@method_decorator(login_required, name="dispatch")
 class DriverListView(generic.ListView):
     model = Driver
     paginate_by = 5
 
 
+@method_decorator(login_required, name="dispatch")
 class DriverDetailView(generic.DetailView):
     model = Driver
     queryset = Driver.objects.prefetch_related("cars__manufacturer")

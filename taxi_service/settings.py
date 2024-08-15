9e3631f929448cf458169d326ed2cb0f288bbed9
AndreiVed <catblues87@gmail.com>
2024-08-15 22:38:44 +0300

    solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -107,6 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = "taxi.Driver"
+LOGIN_REDIRECT_URL = "http://localhost:8000/"
+LOGOUT_REDIRECT_URL = "http://localhost:8000/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/

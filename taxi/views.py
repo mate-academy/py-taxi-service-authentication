7159d057bec3e24e4cd98b1e48571d1405a488f5
TsvetanKichuk <toys4babyodua@gmail.com>
2024-08-17 19:08:26 +0200

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -14,8 +14,8 @@ def index(request):
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
         "num_drivers": num_drivers,
@@ -54,7 +54,7 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
         if current_user.is_authenticated:
             for driver in queryset:
                 if driver == current_user:
-                    driver.username = f"(Me)"
+                    driver.username = "(Me)"
         return queryset
 
 

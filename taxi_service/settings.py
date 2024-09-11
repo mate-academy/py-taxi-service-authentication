34b78144756c7f88b7982995a07adc4fdd270ef1
Ihor Utkin <igorutkin2002@gmail.com>
2024-09-11 16:44:45 +0200

    Resolve without debug toolbar

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
     "taxi",
 ]
 
@@ -51,7 +50,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware"
 ]
 
 ROOT_URLCONF = "taxi_service.urls"
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
 ] + static(
     settings.STATIC_URL,
     document_root=settings.STATIC_ROOT
-) + debug_toolbar_urls()
+)

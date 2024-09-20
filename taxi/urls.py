71d51d74a0782e8c93854636b546fafed3982bbf
Vlad <vlad01bruy@gmail.com>
2024-09-20 13:47:36 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,26 +1,58 @@
-from django.urls import path
+from argparse import Namespace
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
-from .views import (
+from taxi.views import (
     index,
     CarListView,
     CarDetailView,
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
+    user_logout
 )
 
+
 urlpatterns = [
-    path("", index, name="index"),
+    path(
+        "",
+        index,
+        name="index"
+    ),
     path(
         "manufacturers/",
         ManufacturerListView.as_view(),
-        name="manufacturer-list",
+        name="manufacturer-list"
     ),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+        "cars/",
+        CarListView.as_view(),
+        name="car-list"
+    ),
+    path(
+        "cars/<int:pk>/",
+        CarDetailView.as_view(),
+        name="car-detail"
+    ),
+    path(
+        "drivers/",
+        DriverListView.as_view(),
+        name="driver-list"
+    ),
+    path(
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
+    ),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(),
+        name="login"
+    ),
+    path(
+        "logout/",
+        user_logout,
+        name="logout"
     ),
 ]
 

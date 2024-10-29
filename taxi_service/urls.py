45c5a35ab8190605364a0066140dc90b68f63caa
Mykola Kokoiev <veokoknik@gmail.com>
2024-10-29 16:25:21 +0100

    SOL

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 
-urlpatterns = [
+urlpatterns = ([
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("accounts/", include("django.contrib.auth.urls")),
+] + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT
+) + debug_toolbar_urls())

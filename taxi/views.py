1979f0b1630bde1aa6ca7d924fbe0c920b8c9e18
Alex Bul <tarasov57228@gmail.com>
2024-11-03 11:20:48 +0200

    'Solution'

--- taxi/views.py
+++ taxi/views.py
@@ -13,14 +13,14 @@ def index(request):
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
-    num_visit = request.session.get("num_visit", 0)
-    request.session["num_visit"] = num_visit + 1
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
         "num_manufacturers": num_manufacturers,
-        "num_visit": request.session["num_visit"],
+        "num_visits": num_visits + 1,
     }
 
     return render(request, "taxi/index.html", context=context)

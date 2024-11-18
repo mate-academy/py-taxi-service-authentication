2069a6cf1a7b8b16acd0eda3c0e6dd9433410719
Ekaterina <sudarkova.ekaterina@gmail.com>
2024-11-18 10:34:54 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.urls.conf import include
+from django.urls import include
 
 from .views import (
     index,
@@ -23,7 +23,7 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
-    path("accounts/", include("django.contrib.auth.urls"))
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
 app_name = "taxi"
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "taxi_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -130,9 +130,9 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-LOGIN_URL = '/accounts/login/'
+LOGIN_URL = "/accounts/login/"
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = "/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

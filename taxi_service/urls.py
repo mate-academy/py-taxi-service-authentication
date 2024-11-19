7d69dfefdb77d67bbae7bae23da3d89834dc8b08
Bondzik-S <bondziks@gmail.com>
2024-11-19 18:41:30 +0200

    Solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -1,4 +1,5 @@
-"""taxi_service URL Configuration
+"""
+taxi_service URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -23,8 +23,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
-    path("accounts/", include("django.contrib.auth.urls"))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
 
 if settings.DEBUG:
     import debug_toolbar

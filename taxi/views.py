b3c2fa0f454db55a1089d5ecb3bfe3a135a07745
Bondzik-S <bondziks@gmail.com>
2024-11-22 12:00:51 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,8 @@
+from http.client import HTTPResponse
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import generic
 

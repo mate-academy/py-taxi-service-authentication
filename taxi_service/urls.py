803fa16ba3188a6e4012ce3655e97ea5fde18f91
Bondzik-S <bondziks@gmail.com>
2024-11-24 00:15:21 +0200

    Solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -28,7 +28,3 @@ urlpatterns = [
     settings.STATIC_URL,
     document_root=settings.STATIC_ROOT
 )
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]

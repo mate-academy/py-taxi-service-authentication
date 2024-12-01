c144db252e8103eaef395fea0bc0cffb793d417d
Danylo Skepskyi <kol230305@gmail.com>
2024-12-01 18:27:19 +0100

    num_visits fix

--- taxi/views.py
+++ taxi/views.py
@@ -14,7 +14,8 @@ def index(request):
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
+    num_visits += 1
+    request.session["num_visits"] = num_visits
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,

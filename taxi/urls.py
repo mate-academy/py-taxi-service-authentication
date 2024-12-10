b6c368c5cbf306469b6667cb759df8e5cce3d408
Yuriy Komar <omnigun85@gmail.com>
2024-12-10 21:52:13 +0200

    'Solution'

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
 
 from .views import (
-    index,
-    CarListView,
-    CarDetailView,
-    DriverListView,
-    DriverDetailView,
-    ManufacturerListView,
-)
+    index, CarListView, CarDetailView,
+    DriverListView, DriverDetailView, ManufacturerListView)
 
 urlpatterns = [
     path("", index, name="index"),
@@ -18,7 +13,10 @@ urlpatterns = [
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path("drivers/",
+         DriverListView.as_view(),
+         name="driver-list"
+         ),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from taxi.views import logout
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    re_path(r"^logout/$", logout, name="logout")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

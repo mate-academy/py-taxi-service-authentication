f7fe9c030caa4aeffbabb8c3acca44fe46e33d16
ABC <matweyes@gmail.com>
2024-12-10 15:52:19 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
-
 from .models import Driver, Car, Manufacturer
 
 

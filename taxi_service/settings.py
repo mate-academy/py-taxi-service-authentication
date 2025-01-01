3917b2db6feb42aacee00f21f74458e1bc36f70e
Julia Okhrymenko <Ohrimenko15@gmail.com>
2025-01-01 19:02:27 +0200

    'Solution'

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -111,7 +111,7 @@ AUTH_USER_MODEL = "taxi.Driver"
 
 LOGIN_URL = "/login/"
 LOGOUT_URL = "/logout/"
-LOGIN_REDIRECT_URL = "/taxi_service/"
+LOGIN_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/

3a06007eb9bfb36b20883dc4ea94643aaf39b220
Vlad Tymoshko <slradbez@gmail.com>
2025-01-02 17:40:18 +0100

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -23,9 +23,8 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
-    path("login", auth_views.LoginView.as_view(), name="login"),
-    path("logout", auth_views.LogoutView.as_view(), name="logout"),
-
+    path("login/", auth_views.LoginView.as_view(), name="login"),  # Добавлен пробел
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),  # Добавлен пробел
 ]
 
 app_name = "taxi"

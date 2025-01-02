079ae4c70541db3f97311471b8e8c3b72c71ed4f
Vlad Tymoshko <slradbez@gmail.com>
2025-01-02 18:26:42 +0100

    solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import login,logout
+from django.contrib.auth import login, logout
 
 
 urlpatterns = [

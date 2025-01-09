8bbe6dac5d23cf87e38870108aa9c4e8fd51aee2
Глеб Дубинин <gleb1395@gmail.com>
2025-01-09 12:41:40 +0200

    solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),  # NOQA 501
 ]
 
 app_name = "taxi"

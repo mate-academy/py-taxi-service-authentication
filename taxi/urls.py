7d9f310311124d1668bacf6efd6ba8eb51b65776
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2025-01-15 21:45:53 +0100

    adding solution to py-taxi-service-authentication

--- taxi/urls.py
+++ taxi/urls.py
@@ -8,20 +8,17 @@ from .views import (
     DriverDetailView,
     ManufacturerListView,
 )
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("", index, name="index"),
-    path(
-        "manufacturers/",
-        ManufacturerListView.as_view(),
-        name="manufacturer-list",
-    ),
+    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer-list"),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="taxi:index"), name="logout"),
 ]
 
 app_name = "taxi"

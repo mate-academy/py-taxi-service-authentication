cb03ae97504c8b16c29070bf27609f02755e0510
unknown <maximuschampion2002@gmail.com>
2025-02-26 22:44:49 +0200

    taxi-service-authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.contrib.auth import login, logout
 from django.urls import path
 
 urlpatterns = [
-path('login/', login, name='login'),
+    path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
 ]

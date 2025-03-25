eeabe8dc3a643fab545159ead8592efb0b024464
Yasha Lapin <yasha.lapin@gmail.com>
2025-03-25 17:30:58 +0200

    5

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -136,4 +136,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
-LOGIN_URL = "/registration/login/"
+LOGIN_URL = "/login/"
+LOGOUT_URL = "/logout/"

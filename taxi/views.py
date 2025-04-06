ee01d0f4abfe70f33eeeb1ab6228731e502b18f1
Mykhailo Safronov <isevenpwnz@gmail.com>
2025-04-06 11:20:58 +0300

    + fix flake8

--- taxi/views.py
+++ taxi/views.py
@@ -44,7 +44,7 @@ class CarDetailView(LoginRequiredMixin, generic.DetailView):
 class DriverListView(LoginRequiredMixin, generic.ListView):
     model = Driver
     paginate_by = 5
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["user_id"] = self.request.user.id

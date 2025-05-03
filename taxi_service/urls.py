f7d00485e2282803d9a034e5635c095189302bb9
Ngayka <ngayka@gmail.com>
2025-05-03 17:13:19 +0300

    Fixed files

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("taxi.urls", namespace="taxi")),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("taxi", include("taxi.urls", namespace="taxi")),
+    path("accounts/", include("accounts.urls", namespace="accounts")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

78e2c71d244ce685c96c0fd80d800519b918fd4b
Volodymyr Vitsenko <your@mail.com>
2025-05-20 21:06:58 +0200

    taxi service authentication

--- taxi/views.py
+++ taxi/views.py
@@ -6,7 +6,7 @@ from django.views import generic
 from .models import Driver, Car, Manufacturer
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def index(request):
     """View function for the home page of the site."""
 

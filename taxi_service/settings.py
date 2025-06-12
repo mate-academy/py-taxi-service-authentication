5758aec003733b3ef7377e8f0252767b20dab6ce
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-06-12 09:39:54 +0200

    Solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -136,6 +136,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "taxi.Driver"
-
 LOGIN_REDIRECT_URL = reverse_lazy("taxi:index")

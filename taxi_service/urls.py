9509536b49fcb580e54122d50ad1bb3e05f12d38
yuraf <twinkreferal22847228@gmail.com>
2025-06-27 21:41:40 +0300

    solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
-import django.contrib.auth.urls
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
-    path("registration/", include(django.contrib.auth.urls))
+    path("registration/", include("django.contrib.auth.urls"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

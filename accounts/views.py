1d0d9c3f9333f399c31c06d1228cc38e68295f2c
msi <lg.mobile0808@gmail.com>
2025-07-15 00:59:35 +0300

    Solution

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect("taxi:home")
+                return redirect("taxi:index")
     else:
         form = AuthenticationForm()
     return render(request, "registration/login.html", {"form": form})

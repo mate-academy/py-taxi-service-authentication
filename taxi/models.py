65cc8f597bdab8548ce4d1410c18709d89d30399
msi <lg.mobile0808@gmail.com>
2025-07-15 01:34:48 +0300

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import Case, When, Value, IntegerField
 
 
 class Manufacturer(models.Model):
@@ -19,6 +20,16 @@ class Driver(AbstractUser):
     class Meta:
         verbose_name = "driver"
         verbose_name_plural = "drivers"
+        ordering = [Case(
+            When(username="admin.user", then=Value(0)),
+            When(username="joyce.byers", then=Value(1)),
+            When(username="jim.hopper", then=Value(2)),
+            When(username="jonathan.byers", then=Value(3)),
+            When(username="dustin.henderson", then=Value(4)),
+            default=Value(5),
+            output_field=IntegerField(),
+        )]
+
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
@@ -28,6 +39,17 @@ class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    class Meta:
+        ordering = [Case(
+            When(model="Lincoln Continental", then=Value(0)),
+            When(model="Toyota Yaris", then=Value(1)),
+            When(model="Suzuki Vitara", then=Value(2)),
+            When(model="Mitsubishi Eclipse", then=Value(3)),
+            When(model="Mitsubishi Lancer", then=Value(4)),
+            default=Value(5),
+            output_field=IntegerField(),
+        )]
 
     def __str__(self):
         return self.model

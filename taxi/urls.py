d18f66973fea6d1978991238c4a90fb1681f469f
= <=>
2025-07-15 16:57:09 +0300

    implement all this

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,27 +1,14 @@
 from django.urls import path
-
-from .views import (
-    index,
-    CarListView,
-    CarDetailView,
-    DriverListView,
-    DriverDetailView,
-    ManufacturerListView,
-)
+from . import views
 
 urlpatterns = [
-    path("", index, name="index"),
-    path(
-        "manufacturers/",
-        ManufacturerListView.as_view(),
-        name="manufacturer-list",
-    ),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
+    path('', views.index, name='index'),
+    path('manufacturers/', views.ManufacturerListView.as_view(), name='manufacturer-list'),
+    path('manufacturers/<int:pk>/', views.ManufacturerDetailView.as_view(), name='manufacturer-detail'),
+    path('cars/', views.CarListView.as_view(), name='car-list'),
+    path('cars/<int:pk>/', views.CarDetailView.as_view(), name='car-detail'),
+    path('drivers/', views.DriverListView.as_view(), name='driver-list'),
+    path('drivers/<int:pk>/', views.DriverDetailView.as_view(), name='driver-detail'),
 ]
 
-app_name = "taxi"
+app_name = 'taxi'
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("taxi.urls", namespace="taxi")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='taxi/', permanent=True)),
+    path('taxi/', include('taxi.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+]

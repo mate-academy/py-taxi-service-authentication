d9851c63add2eaed8e96392bef46f03e19c06076
Ermolenko Andrey <f1nd6r@gmail.com>
2025-08-02 19:56:37 +0300

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+import django
+from django.urls import path, include
 
 from .views import (
     index,
@@ -19,6 +20,7 @@ urlpatterns = [
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
+
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -133,3 +133,11 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "taxi.Driver"
+
+LOGIN_REDIRECT_URL = "/"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]

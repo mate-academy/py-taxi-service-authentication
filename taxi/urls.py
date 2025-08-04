36f0bbb5f01761240b18e8ca7b538a459892605d
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-08-04 14:45:57 +0200

    taxi service authentification

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from .views import (
     index,
     CarListView,
@@ -7,21 +6,16 @@ from .views import (
     DriverListView,
     DriverDetailView,
     ManufacturerListView,
+
 )
+
+app_name = "taxi"
 
 urlpatterns = [
     path("", index, name="index"),
-    path(
-        "manufacturers/",
-        ManufacturerListView.as_view(),
-        name="manufacturer-list",
-    ),
+    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer-list"),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
-    ),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
 ]
-
-app_name = "taxi"

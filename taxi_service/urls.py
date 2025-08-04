543518a9756ae78ab825d9cc97a42ec33f3f1815
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-08-04 22:15:39 +0200

    Solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -26,5 +26,7 @@ urlpatterns = [
     # path("registration/", include("django.contrib.auth.urls")),
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path("login/",
+         auth_views.LoginView.as_view(template_name='registration/login.html'),
+         name='login'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

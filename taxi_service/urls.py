290e71e75b6307a9c0b3daf28d6f71670d7f1bfd
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-08-04 22:13:52 +0200

    Solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -23,7 +23,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
-    path("registration/", include("django.contrib.auth.urls")),
+    # path("registration/", include("django.contrib.auth.urls")),
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),

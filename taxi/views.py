dab12e8b520238ec0e7c38ce88bfb31364a989f7
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-08-05 10:25:42 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -57,11 +57,11 @@ class DriverDetailView(LoginRequiredMixin, generic.DetailView):
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST.get("username")
+        password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return JsonResponse({'success': True})
+            return JsonResponse({"success": True})
         else:
-            return JsonResponse({'error': 'Invalid credentials'}, status=400)
+            return JsonResponse({"error": "Invalid credentials"}, status=400)

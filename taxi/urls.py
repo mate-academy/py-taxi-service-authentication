655b25e62a6d8eece28d833ec6c9be17a9c1d334
Csillag61 <csillagok@gmail.com>
2025-08-11 15:25:40 -0400

    all bugs fix

--- taxi/urls.py
+++ taxi/urls.py
@@ -10,8 +10,7 @@ from .views import (
     ManufacturerListView,
 )
 from django.contrib.auth import views as auth_views
-from django.contrib.auth import views as auth_views
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path("", index, name="index"),
@@ -24,26 +23,6 @@ urlpatterns = [
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="registration/login.html"),
-        name="login",
-    ),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-]
-
-
-urlpatterns += [
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="registration/login.html"),
-        name="login",
-    ),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-]
-
-
-urlpatterns += [
     path(
         "login/",
         auth_views.LoginView.as_view(template_name="registration/login.html"),

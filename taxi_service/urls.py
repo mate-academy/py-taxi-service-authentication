dfa921fd5353e59aa887c3890a9076b2fcbba7cb
reinvvay <reinvvay@gmail.com>
2025-08-12 19:42:20 +0300

    flake8 fix

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -23,6 +23,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
-    path("accounts/logout/", LogoutView.as_view(template_name="registration/logout.html"), name="logout"),
+    path(
+        "accounts/logout/",
+        LogoutView.as_view(template_name="registration/logout.html"),
+        name="logout"
+    ),
     path("accounts/", include("django.contrib.auth.urls"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

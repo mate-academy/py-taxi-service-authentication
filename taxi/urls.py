003a28374b2fa214ec23c674c3c6c14616c9995e
Andrii Novikov <forytube2023@gmail.com>
2025-08-13 16:20:23 +0300

    Update

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
 from .views import (
     index,
     CarListView,
@@ -32,13 +31,13 @@ urlpatterns = [
 
     # Authentication
     path(
-        "login/",
+        "log-in/",
         auth_views.LoginView.as_view(template_name="registration/login.html"),
-        name="login",
+        name="log-in",
     ),
     path(
-        "logout/",
-        auth_views.LogoutView.as_view(),
-        name="logout",
+        "log-out/",
+        auth_views.LogoutView.as_view(next_page="taxi:log-in"),
+        name="log-out",
     ),
 ]
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -5,6 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
     path("", include("taxi.urls", namespace="taxi")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

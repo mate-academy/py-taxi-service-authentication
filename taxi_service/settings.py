2e853344b23971366a06963d740fcbed09513617
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-08-14 16:17:05 +0300

    fixed

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -142,5 +142,5 @@ INTERNAL_IPS = [
 ]
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/accounts/login/'
-LOGIN_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = '/login/'
+LOGIN_URL = '/login/'
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
-    path('accounts/', include("accounts.urls", namespace="accounts")),
+    path("auth/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:

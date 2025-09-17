5efeb09e8f66a4d0107a51f9b2bdd2b8bd6ae358
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-09-17 20:30:38 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -10,9 +10,9 @@ from .models import Driver, Car, Manufacturer
 def index(request):
     """View function for the home page of the site."""
 
-    num_visits = request.session.get('num_visits', 0)
+    num_visits = request.session.get("num_visits", 0)
     num_visits += 1
-    request.session['num_visits'] = num_visits
+    request.session["num_visits"] = num_visits
 
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -19,9 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("taxi.urls")),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

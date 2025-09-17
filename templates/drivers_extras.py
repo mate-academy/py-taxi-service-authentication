22385583475bf3df94b9ea90d027aa27147383c9
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-09-17 20:50:59 +0200

    Solution

---
+++ templates/drivers_extras.py
@@ -0,0 +1,21 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def get_user_driver_pk(user):
+    """Safely get user's driver pk."""
+    try:
+        if hasattr(user, "driver") and user.driver:
+            return user.driver.pk
+    except:
+        pass
+    return None
+
+@register.simple_tag
+def user_is_driver(user):
+    """Check if user is a driver."""
+    try:
+        return hasattr(user, "driver") and user.driver is not None
+    except:
+        return False

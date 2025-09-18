9ed795e783d43c40129843f4c3de57e5c31854ba
Artem Shlychkin <artemshlychkin1995@gmail.com>
2025-09-18 12:04:26 +0300

    Implement visit counter on home screen, create login\logout screens and buttons also implement authentication

---
+++ taxi/urls.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+from .views import (
+    index,
+    CarListView,
+    CarDetailView,
+    DriverListView,
+    DriverDetailView,
+    ManufacturerListView,
+)
+
+urlpatterns = [
+    path("", index, name="index"),
+    path(
+        "manufacturers/",
+        ManufacturerListView.as_view(),
+        name="manufacturer-list",
+    ),
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path(
+        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+    ),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+]
+
+app_name = "taxi"

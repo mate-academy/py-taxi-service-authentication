58b9be53cd5a7529f3f23c551d95f5b8071543dc
Oleksandr Skoromnov <solalex005@gmail.com>
2025-09-25 15:44:32 +0300

    fix2

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -127,6 +127,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 LOGIN_REDIRECT_URL = "/taxi/"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"

dacbf70931c63322b893ebbeedbd5954f61fbeac
Tomasz Herman <tomekh2015@gmail.com>
2025-10-15 14:43:08 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import (
     index,
@@ -20,8 +21,16 @@ urlpatterns = [
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
-        "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+        "drivers/<int:pk>/",
+        DriverDetailView.as_view(),
+        name="driver-detail"
     ),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="registration/login.html"),
+        name="login"
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
 
 app_name = "taxi"

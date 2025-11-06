03dcda2a154a847bc535931e5433af1088b7bc6e
Samuel Yudi <toyotaa038@gmail.com>
2025-11-06 10:25:34 -0300

    solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,47 +1,69 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
-
+from django.shortcuts import render
 from .models import Driver, Car, Manufacturer
 
 
+@login_required
 def index(request):
-    """View function for the home page of the site."""
-
+    """View da página inicial, com contador de visitas."""
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
+
+    # contador de visitas na sessão
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
         "num_manufacturers": num_manufacturers,
+        "num_visits": num_visits,
     }
+    return render(request, "taxi/index.html", context)
 
-    return render(request, "taxi/index.html", context=context)
 
+# --------- MANUFACTURER ---------
 
-class ManufacturerListView(generic.ListView):
+class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     model = Manufacturer
-    context_object_name = "manufacturer_list"
-    template_name = "taxi/manufacturer_list.html"
-    paginate_by = 5
+    paginate_by = 10
 
 
-class CarListView(generic.ListView):
+class ManufacturerDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Manufacturer
+
+
+# --------- CAR ---------
+
+class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
-    paginate_by = 5
-    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 10
 
 
-class CarDetailView(generic.DetailView):
+class CarDetailView(LoginRequiredMixin, generic.DetailView):
     model = Car
 
 
-class DriverListView(generic.ListView):
+# --------- DRIVER ---------
+
+class DriverListView(LoginRequiredMixin, generic.ListView):
     model = Driver
-    paginate_by = 5
+    paginate_by = 10
+    template_name = "taxi/driver_list.html"
+
+    def get_queryset(self):
+        return Driver.objects.select_related("user").all()
+
+    def get_context_data(self, **kwargs):
+        """Adiciona o usuário logado ao contexto para comparar com os motoristas."""
+        context = super().get_context_data(**kwargs)
+        context["current_user"] = self.request.user
+        return context
 
 
-class DriverDetailView(generic.DetailView):
+class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
-    queryset = Driver.objects.prefetch_related("cars__manufacturer")
+    template_name = "taxi/driver_detail.html"

4a2d5d1c8311757f470b8623169c6598c6ebc0e5
Oleh-Fr <frasyniuk.oleh.dev@gmail.com>
2025-12-02 21:47:21 +0000

    finish

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import (
     index,
@@ -22,6 +23,8 @@ urlpatterns = [
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path('login/', auth_views.LoginView.as_view(template_name='taxi/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='taxi:login'), name='logout'),
 ]
 
 app_name = "taxi"
